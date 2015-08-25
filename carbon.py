16dea2ab5505df0eb91319584b696ed64b48ed10
molebot <molebot@users.noreply.github.com>
2015-08-25 10:19:55 +0800

    Update carbon.py

--- carbon.py
+++ carbon.py
@@ -10,7 +10,7 @@ import requests
 import acc
 
 
-vsn = 'in.2015.08.25.345678'
+vsn = 'in.2015.08.25.3456789'
 
 
 
@@ -135,15 +135,15 @@ class Iron:
         
         uu = -1*zz(0,0,7,-1,q=-2)
         nn = -1*zz(0,0,7, 1,q=-2)
-        if _blue0>100*(1+myth):
-            _blue0 -= (_blue0-(1+myth)*100)*2.0
-        elif _blue0<-100*(1+myth):
-            _blue0 += (-100*(1+myth)-_blue0)*2.0
+#        if _blue0>100*(1+myth):
+#            _blue0 -= (_blue0-(1+myth)*100)*2.0
+#        elif _blue0<-100*(1+myth):
+#            _blue0 += (-100*(1+myth)-_blue0)*2.0
         uuu = _blue0+uu
         nnn = _blue0+nn
 
-        uu = -1*zz(1,0,7,-1,q=-2)
-        nn = -1*zz(1,0,7, 1,q=-2)
+#        uu = -1*zz(1,0,7,-1,q=-2)
+#        nn = -1*zz(1,0,7, 1,q=-2)
 #        uuu=min(uuu, 100*(1+myth))#*2.0
 #        nnn=max(nnn,-100*(1+myth))#*2.0
 #        if uuu<100*myth:
