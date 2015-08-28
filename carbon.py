754fd3f674501584e8a6c4eea1060980db92cdda
molebot <molebot@qq.com>
2015-08-28 09:55:35 +0800

    d11

--- carbon.py
+++ carbon.py
@@ -1,4 +1,4 @@
-vsn = 'in.2015.08.25.d9'
+vsn = 'in.2015.08.25.d11'
 import time,datetime
 from hashlib import md5
 from core import *
@@ -141,8 +141,9 @@ class Iron:
         _blue0 = (_fox0+_just0)/2.0
         _blue = _blue0
 
-        uuu = min( 100*(1+myth),max(_blue1,uuu))
-        nnn = max(-100*(1+myth),min(_blue1,nnn))
+        uuu = 100*(1+myth)-max(0,_blue1-100*(1+myth))#min( 100*(1+myth),max(_blue1,uuu))
+        nnn =-100*(1+myth)+min(0,_blue1+100*(1+myth))#max(-100*(1+myth),min(_blue1,nnn))
+
 
         if passit>=0:
             todo = [passit]
@@ -154,8 +155,8 @@ class Iron:
             c[i][0]['just'] = _blue0# = saved['old'][2][1]
             c[i][0]['uuu'] = uuu
             c[i][0]['nnn'] = nnn
-            c[i][0]['uu'] = uu12*0
-            c[i][0]['nn'] = nn12*0
+            c[i][0]['uu'] = uu12
+            c[i][0]['nn'] = nn12
             c[i][0]['fox'] = _blue1
             self.cache[i][0] = c[i][0]
             self.save(i,c[i][0])
